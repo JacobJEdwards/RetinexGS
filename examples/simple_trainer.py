3d848246027e3623804688a3f9c1462201259791
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-16 21:14:13 +0200

    adaptive curve loss

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -350,7 +350,7 @@ class Runner:
 
         self.loss_color = ColourConsistencyLoss().to(self.device)
         self.loss_color.compile()
-        self.loss_exposure = ExposureLoss(patch_size=32, mean_val=0.7).to(self.device)
+        self.loss_exposure = ExposureLoss(patch_size=32, mean_val=0.62).to(self.device)
         self.loss_exposure.compile()
         self.loss_smooth = SmoothingLoss().to(self.device)
         self.loss_smooth.compile()
