8b254d20c7ba94dc646e071b2a635e630f1618bd
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-16 21:29:50 +0200

    adaptive curve loss

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -163,10 +163,10 @@ class Config:
 
     lambda_reflect: float = 1.0
     lambda_smooth: float = 1.0
-    lambda_illum_color: float = 0.2
-    lambda_illum_exposure: float = 0.6
-    lambda_illum_variance: float = 0.2
-    lambda_illum_contrast: float = 0.5
+    lambda_illum_color: float = 0.8
+    lambda_illum_exposure: float = 1.0
+    lambda_illum_variance: float = 1.0
+    lambda_illum_contrast: float = 0.1
     lambda_adaptive_curve: float = 0.8
     pretrain_retinex: bool = True
     pretrain_steps: int = 2000
