93c5f48fb9d24bef97eeb05fc1865a5e021e6923
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-17 19:30:37 +0200

    laplacian loss

--- examples/config.py
+++ examples/config.py
@@ -116,9 +116,9 @@ class Config:
     lambda_low: float = 0.8
 
     lambda_reflect: float = 4.0
-    lambda_smooth: float = 1.2
+    lambda_smooth: float = 3.2
     lambda_illum_color: float = 0.2
-    lambda_illum_exposure: float = 0.2
+    lambda_illum_exposure: float = 1.2
     lambda_illum_variance: float = 0.05
     lambda_illum_contrast: float = 0.1
     lambda_illum_curve: float = 1.2
