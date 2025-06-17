8b7fbc990454d8692833dc6eb737865073377c55
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-17 14:01:16 +0200

    fixed lambads

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -209,7 +209,7 @@ class Runner:
 
             self.loss_color = ColourConsistencyLoss().to(self.device)
             self.loss_color.compile()
-            self.loss_exposure = ExposureLoss(patch_size=16, mean_val=0.55).to(self.device)
+            self.loss_exposure = ExposureLoss(patch_size=16, mean_val=0.6).to(self.device)
             self.loss_exposure.compile()
             self.loss_smooth = SmoothingLoss().to(self.device)
             self.loss_smooth.compile()
