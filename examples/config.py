89e87fa89fbc8e61a77f9bdf0b8cff781b606417
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-19 10:13:58 +0200

    checkpointing

--- examples/config.py
+++ examples/config.py
@@ -122,7 +122,7 @@ class Config:
     lambda_illum_variance: float = 0.08
     lambda_illum_contrast: float = 0.1
     lambda_illum_curve: float = 1.4
-    pretrain_retinex: bool = True
+    pretrain_retinex: bool = False
     pretrain_steps: int = 4000
     
     use_hsv_color_space: bool = True
