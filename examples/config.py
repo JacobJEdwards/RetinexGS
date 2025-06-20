7c052e7d79f2a2ee50134ea93459bc2020278027
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-20 18:28:28 +0200

    laplachian

--- examples/config.py
+++ examples/config.py
@@ -131,7 +131,7 @@ class Config:
     pretrain_steps: int = 4000
     
     use_hsv_color_space: bool = True
-    use_refinement_net: bool = False
+    use_refinement_net: bool = True
     use_denoising_net: bool = False
     use_denoising_embedding: bool = False
 
