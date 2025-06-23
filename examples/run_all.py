3c92dc02bad1969c5d4ad924c3daeb3ed9aa8b8c
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-06-23 15:30:25 +0200

    leaky relu

---
+++ examples/run_all.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import subprocess
+
+input_dir = Path("../../NeRF_360")
+output_dir = Path("../../result")
+
+for d in input_dir.iterdir():
+    if not d.is_dir():
+        continue
+    
+    print(f"Processing {d.name}...")
+    cmd = [
+        "python",
+        "examples/simple_trainer.py",
+        "default"
+        "--data_dir", str(d),
+        "--result_dir", str(output_dir / d.name),
+    ]
+    
+    print("Running command:", " ".join(cmd))
+    subprocess.run(cmd, check=True)
+    
+print("All datasets processed.")
+    
+
+    
+    
+    
+    
