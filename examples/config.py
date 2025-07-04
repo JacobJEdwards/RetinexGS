dfa3aad50fa62abc1cc73a23fe7c6bb8bab65716
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-04 22:26:04 +0100

    compat

--- examples/config.py
+++ examples/config.py
@@ -138,8 +138,8 @@ class Config:
     use_denoising_embedding: bool = False
     
     predictive_adaptive_curve: bool = False
-    spatial_film: bool = True
-    use_dilated_convs: bool = False
+    spatial_film: bool = False
+    use_dilated_convs: bool = True
     
     use_illum_opt: bool = True
     eval_niqe: bool = False
