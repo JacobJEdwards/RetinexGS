a1c8e4bee241696e5b507ecabe4688f1f1eb22c8
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-05 21:46:16 +0100

    silu

--- examples/retinex.py
+++ examples/retinex.py
@@ -243,7 +243,8 @@ class MultiScaleRetinexNet(nn.Module):
         if self.use_refinement:
             self.refinement_net = RefinementNet(out_channels, out_channels, embed_dim)
 
-        self.relu = nn.PReLU()
+        # self.relu = nn.PReLU()
+        self.relu = nn.SiLU()
         
         self.predictive_adaptive_curve = predictive_adaptive_curve
         if self.predictive_adaptive_curve:
