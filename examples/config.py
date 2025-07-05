d6625aa7638e2b5ded44df880727e7b2c3f6ef2e
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-06 00:12:15 +0100

    dynamic lambdas, illum freq, learnable gamma

--- examples/config.py
+++ examples/config.py
@@ -129,7 +129,7 @@ class Config:
     lambda_gradient: float = 0.01
     lambda_frequency: float = 0
     lambda_edge_aware_smooth: float = 15
-    lambda_illum_frequency: float = 1.0
+    lambda_illum_frequency: float = 0.1
 
     pretrain_retinex: bool = True
     pretrain_steps: int = 5000
