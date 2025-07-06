0a2ead8d267c53175dc47b47649db6e7cc52b6ca
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-06 15:30:42 +0100

    learnable adaptive curve lmabas and contrast

--- examples/config.py
+++ examples/config.py
@@ -149,7 +149,7 @@ class Config:
     use_stride_conv: bool = False
     
     learn_spatial_contrast: bool = False
-    learn_adaptive_curve_lambdas: bool = False
+    learn_adaptive_curve_lambdas: bool = True
     
     use_illum_opt: bool = True
     eval_niqe: bool = False
