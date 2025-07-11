cf034d018a5aa6fe52e01bd4c5fc79eba463c943
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-11 21:28:16 +0100

    config

--- examples/config.py
+++ examples/config.py
@@ -130,7 +130,7 @@ class Config:
 
     learn_spatial_contrast: bool = True
     learn_adaptive_curve_lambdas: bool = False
-    learn_local_exposure: bool = True
+    learn_local_exposure: bool = False
     learn_global_exposure: bool = True
     learn_edge_aware_gamma: bool = False
 
