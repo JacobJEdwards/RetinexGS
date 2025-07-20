d7bed44cd16fd5680302e137986a39c609b28132
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-20 23:46:47 +0100

    script

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -1,7 +1,17 @@
 SCENE_DIR="../../360_v2"
 RESULT_DIR="../../results/benchmark"
 SCENE_LIST="garden bicycle stump bonsai counter kitchen room" # treehill flowers
+RESULT_DIR_BASE="../../results/benchmark"
 RENDER_TRAJ_PATH="ellipse"
+
+CONFIG_OPTIONS=(
+    "--learn_edge_aware_gamma"                  # example config 1
+    "--no-learn_spatial_contrast --no-learn_adaptive_curve_lambdas --no-learn_local_exposure --no-learn_global_exposure"
+    "--use_dynamic_weights"
+    "--predictive_adaptive_curve"
+    "--illum_opt_type base"
+    "--no-use_illum_opt"
+)
 
 for SCENE in $SCENE_LIST;
 do
@@ -18,35 +28,28 @@ do
         --data_dir $SCENE_DIR/"$SCENE"/ \
         --result_dir $RESULT_DIR/"$SCENE"/
 
-#    # run eval and render
-#    for CKPT in "$RESULT_DIR"/"$SCENE"/ckpts/*;
-#    do
-#        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-#            --render_traj_path $RENDER_TRAJ_PATH \
-#            --data_dir $SCENE_DIR/"$SCENE"/ \
-#            --result_dir $RESULT_DIR/"$SCENE"/ \
-#            --ckpt "$CKPT"
-#    done
 done
 
+CONFIG_INDEX=1
 
-for SCENE in $SCENE_LIST;
-do
-    echo "=== Eval Stats ==="
+for CONFIG in "${CONFIG_OPTIONS[@]}"; do
+    CONFIG_RESULT_DIR="$RESULT_DIR_BASE/result$CONFIG_INDEX"
 
-    for STATS in "$RESULT_DIR"/"$SCENE"/stats/val*.json;
-    do  
-        echo "$STATS"
-        cat "$STATS";
-        echo
+    for SCENE in $SCENE_LIST; do
+        if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+            DATA_FACTOR=2
+        else
+            DATA_FACTOR=4
+        fi
+
+        echo "Running scene: $SCENE with config $CONFIG (result dir: result$CONFIG_INDEX)"
+
+        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+            --render_traj_path $RENDER_TRAJ_PATH \
+            --data_dir "$SCENE_DIR/$SCENE/" \
+            --result_dir "$CONFIG_RESULT_DIR/$SCENE/" \
+            "$CONFIG"
     done
 
-    echo "=== Train Stats ==="
-
-    for STATS in "$RESULT_DIR"/"$SCENE"/stats/train*_rank0.json;
-    do  
-        echo "$STATS"
-        cat "$STATS";
-        echo
-    done
+    ((CONFIG_INDEX++))
 done
