e570a576a7897e6b9425b8a39fde3063002ef68d
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-20 23:55:21 +0100

    config

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -18,22 +18,26 @@ CONFIG_OPTIONS=(
     "--lambda_gray_world 0 --lambda_tv_loss 0 --lambda_illum_smoothness 0.001"
 )
 
-for SCENE in $SCENE_LIST;
-do
-    if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
-        DATA_FACTOR=2
-    else
-        DATA_FACTOR=4
-    fi
-
-    echo "Running $SCENE"
-
-#     train without eval
-    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-        --render_traj_path $RENDER_TRAJ_PATH \
-        --data_dir $SCENE_DIR/"$SCENE"/ \
-        --result_dir $RESULT_DIR/"$SCENE"/
-done
+#for SCENE in $SCENE_LIST;
+#do
+#    if [ "$SCENE" = "bicycle" ] || [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "garden" ] || [ "$SCENE" = "stump" ]; then
+#      continue
+#    fi
+#
+#    if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+#        DATA_FACTOR=2
+#    else
+#        DATA_FACTOR=4
+#    fi
+#
+#    echo "Running $SCENE"
+#
+##     train without eval
+#    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+#        --render_traj_path $RENDER_TRAJ_PATH \
+#        --data_dir $SCENE_DIR/"$SCENE"/ \
+#        --result_dir $RESULT_DIR/"$SCENE"/
+#done
 
 CONFIG_INDEX=1
 
