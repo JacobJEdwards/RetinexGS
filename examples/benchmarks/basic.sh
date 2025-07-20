8ec889f8a7ba143315676450fb0285a94becf755
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-20 23:41:59 +0100

    script

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -3,6 +3,14 @@ RESULT_DIR="../../results/benchmark"
 SCENE_LIST="garden bicycle stump bonsai counter kitchen room" # treehill flowers
 RENDER_TRAJ_PATH="ellipse"
 
+RESULT_DIR_BASE="../../results/benchmark"
+
+CONFIG_OPTIONS=(
+    "--option1 value1"                  # example config 1
+    "--option2 value2 --flag"          # example config 2
+    "--option3"                         # example config 3 (flag only)
+)
+
 for SCENE in $SCENE_LIST;
 do
     if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
@@ -18,36 +26,28 @@ do
         --render_traj_path $RENDER_TRAJ_PATH \
         --data_dir $SCENE_DIR/"$SCENE"/ \
         --result_dir $RESULT_DIR/"$SCENE"/
-
-    # run eval and render
-#    for CKPT in "$RESULT_DIR"/"$SCENE"/ckpts/*;
-#    do
-#        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-#            --render_traj_path $RENDER_TRAJ_PATH \
-#            --data_dir $SCENE_DIR/"$SCENE"/ \
-#            --result_dir $RESULT_DIR/"$SCENE"/ \
-#            --ckpt "$CKPT"
-#    done
 done
 
+CONFIG_INDEX=1
 
-for SCENE in $SCENE_LIST;
-do
-    echo "=== Eval Stats ==="
+for CONFIG in "${CONFIG_OPTIONS[@]}"; do
+    CONFIG_RESULT_DIR="$RESULT_DIR_BASE/result$CONFIG_INDEX"
 
-    for STATS in "$RESULT_DIR"/"$SCENE"/stats/val*.json;
-    do  
-        echo "$STATS"
-        cat "$STATS";
-        echo
-    done
+    for SCENE in $SCENE_LIST; do
+            if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+                DATA_FACTOR=2
+            else
+                DATA_FACTOR=4
+            fi
 
-    echo "=== Train Stats ==="
+            echo "Running scene: $SCENE with config $CONFIG (result dir: result$CONFIG_INDEX)"
 
-    for STATS in "$RESULT_DIR"/"$SCENE"/stats/train*_rank0.json;
-    do  
-        echo "$STATS"
-        cat "$STATS";
-        echo
-    done
+            CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+                --render_traj_path $RENDER_TRAJ_PATH \
+                --data_dir "$SCENE_DIR/$SCENE/" \
+                --result_dir "$CONFIG_RESULT_DIR/$SCENE/" \
+                "$CONFIG"
+        done
+
+        ((CONFIG_INDEX++))
 done
