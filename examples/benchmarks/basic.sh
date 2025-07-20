43a6cd88a005567466731dc7184461575cd08097
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-20 15:53:45 +0100

    normal eval

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -13,21 +13,20 @@ do
 
     echo "Running $SCENE"
 
-#     train without eval
-    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --eval_steps -1 --disable_viewer --data_factor $DATA_FACTOR \
+    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
         --render_traj_path $RENDER_TRAJ_PATH \
         --data_dir $SCENE_DIR/"$SCENE"/ \
         --result_dir $RESULT_DIR/"$SCENE"/
 
-    # run eval and render
-    for CKPT in "$RESULT_DIR"/"$SCENE"/ckpts/*;
-    do
-        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-            --render_traj_path $RENDER_TRAJ_PATH \
-            --data_dir $SCENE_DIR/"$SCENE"/ \
-            --result_dir $RESULT_DIR/"$SCENE"/ \
-            --ckpt "$CKPT"
-    done
+#    # run eval and render
+#    for CKPT in "$RESULT_DIR"/"$SCENE"/ckpts/*;
+#    do
+#        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+#            --render_traj_path $RENDER_TRAJ_PATH \
+#            --data_dir $SCENE_DIR/"$SCENE"/ \
+#            --result_dir $RESULT_DIR/"$SCENE"/ \
+#            --ckpt "$CKPT"
+#    done
 done
 
 
