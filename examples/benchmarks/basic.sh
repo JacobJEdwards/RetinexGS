0797b53ba79834771cf1114169d778b7e325c915
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-21 00:10:41 +0100

    config

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -13,26 +13,26 @@ CONFIG_OPTIONS=(
     "--no-use_illum_opt"
 )
 
-#for SCENE in $SCENE_LIST;
-#do
-#    if [ "$SCENE" = "bicycle" ] || [ "$SCENE" = "garden" ]; then
-#      continue
-#    fi
-#
-#    if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
-#        DATA_FACTOR=2
-#    else
-#        DATA_FACTOR=4
-#    fi
-#
-#    echo "Running $SCENE"
-#
-#    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-#        --render_traj_path $RENDER_TRAJ_PATH \
-#        --data_dir $SCENE_DIR/"$SCENE"/ \
-#        --result_dir $RESULT_DIR/"$SCENE"/
-#
-#done
+for SCENE in $SCENE_LIST;
+do
+    if [ "$SCENE" = "bicycle" ] || [ "$SCENE" = "garden" ]; then
+      continue
+    fi
+
+    if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+        DATA_FACTOR=2
+    else
+        DATA_FACTOR=4
+    fi
+
+    echo "Running $SCENE"
+
+    CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+        --render_traj_path $RENDER_TRAJ_PATH \
+        --data_dir $SCENE_DIR/"$SCENE"/ \
+        --result_dir $RESULT_DIR/"$SCENE"/
+
+done
 
 CONFIG_INDEX=1
 
