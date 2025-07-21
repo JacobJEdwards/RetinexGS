c3ac9f5f2000be43a790d29d99586f46b589906f
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-21 12:09:58 +0100

    use variance

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -1,29 +1,12 @@
 SCENE_DIR="../../360_v2"
-RESULT_DIR="../../results/benchmark"
+RESULT_DIR="../../results/benchmark/variance"
 SCENE_LIST="garden bicycle stump bonsai counter kitchen room" # treehill flowers
 RENDER_TRAJ_PATH="ellipse"
 
-RESULT_DIR_BASE="../../results/benchmark"
-
-CONFIG_OPTIONS=(
-    "--lambda_shn_reg 0 --lambda_gray_world 0"
-    "--lambda_shn_reg 0 --lambda_gray_world 0 --lambda_tv_loss 0 --lambda_exclusion 0"
-    "--lambda_shn_reg 0 --lambda_gray_world 0 --appearance_embeddings"
-    "--lambda_shn_reg 0 --lambda_gray_world 0 --decomposed_field"
-    " --lambda_shn_reg 0 --lambda_gray_world 0 --decomposed_field --appearance_embeddings"
-    " --lambda_shn_reg 0 --lambda_gray_world 0 --lambda_tv_loss 0"
-    " --lambda_shn_reg 0 --lambda_gray_world 0 --lambda_tv_loss 0 --lambda_illum_smoothness 0.001"
-    " --lambda_shn_reg 0 --lambda_tv_loss 0"
-    " --lambda_gray_world 0"
-    " --lambda_gray_world 0 --lambda_tv_loss 0 --lambda_illum_smoothness 0.001"
-)
+RESULT_DIR_BASE="../../results/benchmark/variance"
 
 for SCENE in $SCENE_LIST;
 do
-    if [ "$SCENE" = "bicycle" ] || [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "garden" ] || [ "$SCENE" = "stump" ]; then
-      continue
-    fi
-
     if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
         DATA_FACTOR=2
     else
@@ -32,34 +15,8 @@ do
 
     echo "Running $SCENE"
 
-#     train without eval
     CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
         --render_traj_path $RENDER_TRAJ_PATH \
         --data_dir $SCENE_DIR/"$SCENE"/ \
         --result_dir $RESULT_DIR/"$SCENE"/
-done
-
-CONFIG_INDEX=1
-
-for CONFIG in "${CONFIG_OPTIONS[@]}"; do
-    CONFIG_RESULT_DIR="$RESULT_DIR_BASE/result$CONFIG_INDEX"
-
-    for SCENE in $SCENE_LIST; do
-            if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
-                DATA_FACTOR=2
-            else
-                DATA_FACTOR=4
-            fi
-
-            echo "Running scene: $SCENE with config $CONFIG (result dir: result$CONFIG_INDEX)"
-
-
-            CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-                --render_traj_path $RENDER_TRAJ_PATH \
-                --data_dir "$SCENE_DIR/$SCENE/" \
-                --result_dir "$CONFIG_RESULT_DIR/$SCENE/" \
-                $CONFIG
-        done
-
-        ((CONFIG_INDEX++))
 done
