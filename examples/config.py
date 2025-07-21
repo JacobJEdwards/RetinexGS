a1835bc98513d028bfca2a47b5fe6462d7f709fc
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-21 18:30:42 +0100

    eval

--- examples/config.py
+++ examples/config.py
@@ -12,7 +12,7 @@ class Config:
     compression: Literal["png"] | None = None
     render_traj_path: str = "interp"
 
-    data_dir: Path = Path("../../360_v2/bonsai")
+    data_dir: Path = Path("../../360_v2/bicycle")
     data_factor: int = 1
     result_dir: Path = Path("../../result")
     test_every: int = 8
