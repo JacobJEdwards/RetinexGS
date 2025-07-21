849b66e4e4f38e62e85db3b899673e0360941a12
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-21 14:36:53 +0100

    scropt

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -30,30 +30,10 @@ do
     CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
         --render_traj_path $RENDER_TRAJ_PATH \
         --data_dir $SCENE_DIR/"$SCENE"/ \
-        --result_dir $RESULT_DIR/"$SCENE"/
+        --result_dir $RESULT_DIR/"$SCENE"/ \
+        --no_learn_spatial_contrast \
+         --no-learn_adaptive_curve_lambdas \
+         --no-learn_local_exposure \
+         --no-learn_global_exposure
 
-done
-
-CONFIG_INDEX=1
-
-for CONFIG in "${CONFIG_OPTIONS[@]}"; do
-    CONFIG_RESULT_DIR="$RESULT_DIR_BASE/result$CONFIG_INDEX"
-
-    for SCENE in $SCENE_LIST; do
-        if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
-            DATA_FACTOR=2
-        else
-            DATA_FACTOR=4
-        fi
-
-        echo "Running scene: $SCENE with config $CONFIG (result dir: result$CONFIG_INDEX)"
-
-        CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-            --render_traj_path $RENDER_TRAJ_PATH \
-            --data_dir "$SCENE_DIR/$SCENE/" \
-            --result_dir "$CONFIG_RESULT_DIR/$SCENE/" \
-            $CONFIG
-    done
-
-    ((CONFIG_INDEX++))
 done
