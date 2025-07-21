c4b0438e5a921547c7c402233f4c56621f5fc056
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-07-21 15:02:17 +0100

    prevent overfitting (hopefully)

--- examples/benchmarks/basic.sh
+++ examples/benchmarks/basic.sh
@@ -1,9 +1,7 @@
 SCENE_DIR="../../360_v2"
-RESULT_DIR="../../results/benchmark/variance"
+RESULT_DIR="../../results/benchmark/attempt2"
 SCENE_LIST="garden bicycle stump bonsai counter kitchen room" # treehill flowers
 RENDER_TRAJ_PATH="ellipse"
-
-RESULT_DIR_BASE="../../results/benchmark/variance"
 
 for SCENE in $SCENE_LIST;
 do
