3b942d4f44c9fa305b860f3acc256679b922a4cb
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-15 10:46:57 +0100

    yuv colour space

--- examples/config.py
+++ examples/config.py
@@ -76,10 +76,10 @@ class Config:
     postfix = "_variance"
 
     lambda_illum_smoothness: float = 1.0
-    lambda_exclusion: float = 0.05
+    lambda_exclusion: float = 0.5
     lambda_reflectance_reg: float = 0.2
-    lambda_shn_reg: float = 0.1
-    lambda_gray_world: float = 0.5
+    lambda_shn_reg: float = 0.2
+    lambda_gray_world: float = 1.0
     lambda_tv_loss: float = 0.2
     lambda_camera_reg: float = 0.1
 
