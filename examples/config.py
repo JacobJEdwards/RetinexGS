73eeaad1c1d0ea87d768df21507da4077afb9cf6
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-17 09:57:05 +0100

    no hsv

--- examples/config.py
+++ examples/config.py
@@ -81,7 +81,7 @@ class Config:
     pretrain_retinex: bool = True
     pretrain_steps: int = 5000
 
-    use_hsv_color_space: bool = False
+    use_hsv_color_space: bool = True
 
     predictive_adaptive_curve: bool = True
 
