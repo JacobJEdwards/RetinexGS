4fe9b613f1cd53f687a65da859f8fab283f63d17
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-17 09:55:42 +0100

    no hsv

--- examples/config.py
+++ examples/config.py
@@ -73,7 +73,7 @@ class Config:
     lambda_white_preservation: float = 0.01
     lambda_histogram: float = 0.8
     
-    lambda_illum_color: float = 10.0
+    lambda_illum_color: float = 1.0
     lambda_illum_exposure_local: float = 0.1
 
     lambda_bidirectional: float = 0.5
