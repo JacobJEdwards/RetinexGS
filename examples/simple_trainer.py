dffdd0c403e01b23c6c98af1dddb0781f7248601
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-19 23:21:06 +0100

    optim

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -1436,7 +1436,7 @@ if __name__ == "__main__":
     #
     study = optuna.create_study(directions=["maximize", "maximize", "minimize"])
 
-    study.optimize(objective, n_trials=50, catch=(RuntimeError,))
+    study.optimize(objective, n_trials=30, catch=(RuntimeError,))
 
     print("Study statistics: ")
     print(f"  Number of finished trials: {len(study.trials)}")
