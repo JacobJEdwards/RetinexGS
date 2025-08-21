28045d78b176b51b3f330b309f14487440d7d50f
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-21 10:32:39 +0200

    hp

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -396,11 +396,9 @@ class Runner:
         else:
             reflectance_map = torch.exp(log_reflectance_target)
 
-
-
         # reflectance_map = torch.clamp(reflectance_map, 0.0, 1.0)
-        # reflectance_map.nan_to_num()
-
+        reflectance_map = reflectance_map.nan_to_num()
+        #
         # reflectance_map_clahe = kornia.enhance.equalize_clahe(
         #     reflectance_map,
         #     clip_limit=2.0,
