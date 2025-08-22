1adbbaa312e295b434f2af3ef467c357d421ad1a
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-22 18:36:20 +0200

    learnable white preseravation

--- examples/config.py
+++ examples/config.py
@@ -87,7 +87,7 @@ class Config:
 
     predictive_adaptive_curve: bool = True
 
-    learn_spatial_contrast: bool = False
+    learn_spatial_contrast: bool = True
     learn_adaptive_curve_lambdas: bool = True
     learn_local_exposure: bool = True
     learn_global_exposure: bool = True
