46eb6486156fb28d3387d9460689d6b168eb4a52
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-22 16:15:41 +0200

    optim

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -1534,48 +1534,30 @@ def objective_ray(config: dict):
 def objective(trial: optuna.Trial):
     cfg = Config()
 
-    # cfg.lambda_reflect = trial.suggest_float("lambda_reflect", 0.0, 5.0)
-    # cfg.lambda_illum_curve = trial.suggest_float("lambda_illum_curve", 1e-2, 10.0, log=True)
-    # cfg.lambda_illum_exposure = trial.suggest_float("lambda_illum_exposure", 0.0, 4.0)
-    # cfg.lambda_edge_aware_smooth = trial.suggest_float("lambda_edge_aware_smooth", 1, 50.0, log=True)
-    # cfg.lambda_illum_exposure_local = trial.suggest_float("lambda_illum_exposure_local", 0.0, 2.0)
-    cfg.lambda_white_preservation = trial.suggest_float("lambda_white_preservation", 1e-2, 10.0, log=True)
-    # cfg.lambda_histogram = trial.suggest_float("lambda_histogram", 1e-2, 10.0, log=True)
-    # cfg.lambda_illum_exclusion = trial.suggest_float("lambda_illum_exclusion", 0.0, 2.0)
+    cfg.retinex_opt_lr = trial.suggest_float("retinex_opt_lr", 1e-5, 1e-2, log=True)
+    cfg.retinex_embedding_lr = trial.suggest_float("retinex_embedding_lr", 1e-5, 1e-2, log=True)
+    cfg.retinex_embedding_dim = trial.suggest_categorical("retinex_embedding_dim", [32, 64])
 
-    # cfg.retinex_opt_lr = trial.suggest_float("retinex_opt_lr", 1e-5, 1e-2, log=True)
-    # cfg.retinex_embedding_lr = trial.suggest_float("retinex_embedding_lr", 1e-5, 1e-2, log=True)
-    # cfg.retinex_embedding_dim = trial.suggest_categorical("retinex_embedding_dim", [32, 64])
-
-    # cfg.learn_spatial_contrast = trial.suggest_categorical(
-    #     "learn_spatial_contrast", [True, False]
-    # )
-    # cfg.learn_global_exposure = trial.suggest_categorical(
-    #     "learn_global_exposure", [True, False]
-    # )
-    # cfg.learn_local_exposure = trial.suggest_categorical(
-    #     "learn_local_exposure", [True, False]
-    # )
-    # cfg.learn_edge_aware_gamma = trial.suggest_categorical(
-    #     "learn_edge_aware_gamma", [True, False]
-    # )
-    # cfg.predictive_adaptive_curve = trial.suggest_categorical(
-    #     "predictive_adaptive_curve", [True, False]
-    # )
-
-    cfg.luminance_threshold = trial.suggest_float(
-        "luminance_threshold", 75, 100
+    cfg.learn_spatial_contrast = trial.suggest_categorical(
+        "learn_spatial_contrast", [True, False]
     )
-    cfg.chroma_tolerance = trial.suggest_float(
-        "chroma_tolerance", 0.01, 20, log=True
+    cfg.learn_global_exposure = trial.suggest_categorical(
+        "learn_global_exposure", [True, False]
     )
-    cfg.gain = trial.suggest_float("gain", 0.5, 30.0, log=True)
-
-
+    cfg.learn_local_exposure = trial.suggest_categorical(
+        "learn_local_exposure", [True, False]
+    )
+    cfg.learn_edge_aware_gamma = trial.suggest_categorical(
+        "learn_edge_aware_gamma", [True, False]
+    )
+    cfg.predictive_adaptive_curve = trial.suggest_categorical(
+        "predictive_adaptive_curve", [True, False]
+    )
 
     cfg.max_steps = 3000
     cfg.eval_steps = [3000]
     cfg.pretrain_steps = 2000
+    cfg.pretrain_retinex = True
 
     average_psnr = 0.0
     average_ssim = 0.0
@@ -1637,23 +1619,23 @@ if __name__ == "__main__":
     config.adjust_steps(config.steps_scaler)
     torch.set_float32_matmul_precision("high")
 
-    cli(main, config, verbose=True)
+    # cli(main, config, verbose=True)
 
-    # study = optuna.create_study(directions=["maximize", "maximize", "minimize"])
-    #
-    # study.optimize(objective, n_trials=30, catch=(RuntimeError,))
-    #
-    # print("Study statistics: ")
-    # print(f" Number of finished trials: {len(study.trials)}")
-    #
-    # print("Best trials (Pareto front):")
-    # for i, trial in enumerate(study.best_trials):
-    #     print(f" Trial {i}:")
-    # print(f" Values: PSNR={trial.values[0]:.4f}, SSIM={trial.values[1]:.4f}, LPIPS={trial.values[2]:.4f}")
-    # print(" Params: ")
-    # for key, value in trial.params.items():
-    #     print(f" {key}: {value}")
-    #
-    # # save the top results to a file
-    # with open("optuna_results_stump.json", "w") as f:
-    #     json.dump(study.trials_dataframe().to_dict(orient="records"), f, indent=4)
+    study = optuna.create_study(directions=["maximize", "maximize", "minimize"])
+
+    study.optimize(objective, n_trials=30, catch=(RuntimeError,))
+
+    print("Study statistics: ")
+    print(f" Number of finished trials: {len(study.trials)}")
+
+    print("Best trials (Pareto front):")
+    for i, trial in enumerate(study.best_trials):
+        print(f" Trial {i}:")
+        print(f" Values: PSNR={trial.values[0]:.4f}, SSIM={trial.values[1]:.4f}, LPIPS={trial.values[2]:.4f}")
+        print(" Params: ")
+        for key, value in trial.params.items():
+            print(f" {key}: {value}")
+
+    # save the top results to a file
+    with open("optuna_results_stump.json", "w") as f:
+        json.dump(study.trials_dataframe().to_dict(orient="records"), f, indent=4)
