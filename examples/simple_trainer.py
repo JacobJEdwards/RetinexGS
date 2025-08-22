2bbf7d647998347a847db15683213801e9e6b089
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-22 15:20:18 +0200

    dynamic weighting

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -233,11 +233,24 @@ class Runner:
         if world_size > 1:
             self.retinex_net = DDP(self.retinex_net, device_ids=[local_rank])
 
+        self.log_sigmas = nn.ParameterDict({
+            "reflect_spa": nn.Parameter(torch.zeros(1)),
+            "color_val": nn.Parameter(torch.zeros(1)),
+            "exposure_val": nn.Parameter(torch.zeros(1)),
+            "adaptive_curve": nn.Parameter(torch.zeros(1)),
+            "smooth_edge_aware": nn.Parameter(torch.zeros(1)),
+            "exposure_local": nn.Parameter(torch.zeros(1)),
+            "exclusion_val": nn.Parameter(torch.zeros(1)),
+            "white_preservation": nn.Parameter(torch.zeros(1)),
+            "histogram_loss": nn.Parameter(torch.zeros(1)),
+        }).to(self.device)
+
         net_params = list(self.retinex_net.parameters())
 
         net_params += self.loss_edge_aware_smooth.parameters()
         net_params += self.loss_adaptive_curve.parameters()
         net_params += self.loss_spatial.parameters()
+        net_params += self.log_sigmas.parameters()
         net_params.append(self.global_mean_val_param)
         net_params.append(self.target_histogram_dist)
 
@@ -484,37 +497,56 @@ class Runner:
 
         loss_histogram = self.histogram_loss(reflectance_map, self.target_histogram_dist)
 
+        individual_losses = {
+            "reflect_spa": loss_reflectance_spa,
+            "color_val": loss_color_val,
+            "exposure_val": loss_exposure_val,
+            "adaptive_curve": loss_adaptive_curve,
+            "smooth_edge_aware": loss_smooth_edge_aware,
+            "exposure_local": loss_exposure_local,
+            "exclusion_val": loss_exclusion_val,
+            "white_preservation": loss_white_preservation,
+            "histogram_loss": loss_histogram,
+        }
 
-        individual_losses = torch.stack(
-            [
-                loss_reflectance_spa,  # 0
-                loss_color_val,  # 1
-                loss_exposure_val,  # 2
-                loss_adaptive_curve,  # 4
-                loss_smooth_edge_aware,  # 8
-                loss_exposure_local,  # 9
-                loss_exclusion_val,  # 11
-                loss_white_preservation,  # 12
-                loss_histogram,  # 13
-            ]
-        )
+        total_loss = 0
+        loss_names = individual_losses.keys()
+        for name in loss_names:
+            loss = individual_losses[name]
+            log_sigma = self.log_sigmas[name]
+            total_loss += 0.5 * torch.exp(-log_sigma) * loss + 0.5 * log_sigma
 
-        base_lambdas = torch.tensor(
-            [
-                cfg.lambda_reflect,
-                cfg.lambda_illum_color,
-                cfg.lambda_illum_exposure,
-                cfg.lambda_illum_curve,
-                cfg.lambda_edge_aware_smooth,
-                cfg.lambda_illum_exposure_local,
-                cfg.lambda_illum_exclusion,
-                cfg.lambda_white_preservation,
-                cfg.lambda_histogram,
-            ],
-            device=device,
-        )
-
-        total_loss = (base_lambdas * individual_losses).sum()
+        #
+        # individual_losses = torch.stack(
+        #     [
+        #         loss_reflectance_spa,  # 0
+        #         loss_color_val,  # 1
+        #         loss_exposure_val,  # 2
+        #         loss_adaptive_curve,  # 4
+        #         loss_smooth_edge_aware,  # 8
+        #         loss_exposure_local,  # 9
+        #         loss_exclusion_val,  # 11
+        #         loss_white_preservation,  # 12
+        #         loss_histogram,  # 13
+        #     ]
+        # )
+        #
+        # base_lambdas = torch.tensor(
+        #     [
+        #         cfg.lambda_reflect,
+        #         cfg.lambda_illum_color,
+        #         cfg.lambda_illum_exposure,
+        #         cfg.lambda_illum_curve,
+        #         cfg.lambda_edge_aware_smooth,
+        #         cfg.lambda_illum_exposure_local,
+        #         cfg.lambda_illum_exclusion,
+        #         cfg.lambda_white_preservation,
+        #         cfg.lambda_histogram,
+        #     ],
+        #     device=device,
+        # )
+        #
+        # total_loss = (base_lambdas * individual_losses).sum()
 
         if step % self.cfg.tb_every == 0:
             self.writer.add_scalar("retinex_net/total_loss", total_loss.item(), step)
@@ -533,15 +565,14 @@ class Runner:
 
             title = "retinex_net" if is_pretrain else "train"
 
-            for i, name in enumerate(loss_names):
+            for name in loss_names:
+                unweighted_loss = individual_losses[name].item()
+                learned_weight = torch.exp(-self.log_sigmas[name]).item()
                 self.writer.add_scalar(
-                    f"{title}/loss_{name}_unweighted", individual_losses[i].item(), step
+                    f"{title}/loss_{name}_unweighted", unweighted_loss, step
                 )
-
                 self.writer.add_scalar(
-                    f"{title}/loss_{name}_fixed_weighted",
-                    (individual_losses[i] * base_lambdas[i]).item(),
-                    step,
+                    f"{title}/learned_weight_{name}", learned_weight, step
                 )
 
             # if cfg.learn_edge_aware_gamma:
