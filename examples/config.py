262a071a33d9c85bafbd97cfd21f8dd649465f8a
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-23 13:21:43 +0200

    no local exposure

--- examples/config.py
+++ examples/config.py
@@ -12,7 +12,7 @@ class Config:
     compression: Literal["png"] | None = None
     render_traj_path: str = "interp"
 
-    data_dir: Path = Path("/workspace/360_v2/kitchen")
+    data_dir: Path = Path("/workspace/360_v2/bicycle")
     data_factor: int = 1
     result_dir: Path = Path("/workspace/result")
     test_every: int = 8
