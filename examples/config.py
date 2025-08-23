3c4577edd22356885144669114c7d7ed30ce8536
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-23 15:07:21 +0200

    revert!

--- examples/config.py
+++ examples/config.py
@@ -80,7 +80,7 @@ class Config:
 
     lambda_illum_color: float = 1.
 
-    pretrain_retinex: bool = False
+    pretrain_retinex: bool = True
     pretrain_steps: int = 1000
 
     use_hsv_color_space: bool = True
@@ -104,7 +104,7 @@ class Config:
 
     retinex_embedding_dim: int = 32
 
-    freeze_step: int = 2000
+    freeze_step: int = 10
 
     def adjust_steps(self, factor: float) -> None:
         self.eval_steps = [int(i * factor) for i in self.eval_steps]
