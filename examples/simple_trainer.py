a0bc6fe12483954f634168b65e628c1417542040
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-24 13:54:52 +0200

    spaitally film is gone

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -1651,19 +1651,19 @@ if __name__ == "__main__":
     torch.set_float32_matmul_precision("high")
 
     cli(main, config, verbose=True)
-
-    study = optuna.create_study(directions=["maximize", "maximize", "minimize"])
-
-    study.optimize(objective2, n_trials=60, catch=(RuntimeError, ValueError))
-
-    print("Study statistics: ")
-    print(f" Number of finished trials: {len(study.trials)}")
-
-    print("Best trials (Pareto front):")
-    for i, trial in enumerate(study.best_trials):
-        print(f" Trial {i}:")
-        print(f" Values: PSNR={trial.values[0]:.4f}, SSIM={trial.values[1]:.4f}, LPIPS={trial.values[2]:.4f}")
-        print(" Params: ")
-        for key, value in trial.params.items():
-            print(f" {key}: {value}")
-
+    #
+    # study = optuna.create_study(directions=["maximize", "maximize", "minimize"])
+    #
+    # study.optimize(objective2, n_trials=60, catch=(RuntimeError, ValueError))
+    #
+    # print("Study statistics: ")
+    # print(f" Number of finished trials: {len(study.trials)}")
+    #
+    # print("Best trials (Pareto front):")
+    # for i, trial in enumerate(study.best_trials):
+    #     print(f" Trial {i}:")
+    #     print(f" Values: PSNR={trial.values[0]:.4f}, SSIM={trial.values[1]:.4f}, LPIPS={trial.values[2]:.4f}")
+    #     print(" Params: ")
+    #     for key, value in trial.params.items():
+    #         print(f" {key}: {value}")
+    #
