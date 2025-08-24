e5577fd0ac3ed5d4381ec036aa01e5e323fc9b16
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-24 13:53:42 +0200

    spaitally film is gone

--- examples/retinex.py
+++ examples/retinex.py
@@ -178,8 +178,8 @@ class MultiScaleRetinexNet(nn.Module):
 
         self.in_conv = RetinexBlock(in_channels, 16)
 
-        # self.film1 = SpatiallyFiLMLayer(embed_dim=embed_dim, feature_channels=16)
-        self.film1 = FiLMLayer(embed_dim=embed_dim, feature_channels=16)
+        self.film1 = SpatiallyFiLMLayer(embed_dim=embed_dim, feature_channels=16)
+        # self.film1 = FiLMLayer(embed_dim=embed_dim, feature_channels=16)
 
         self.enc1 = RetinexBlock(16, 32, stride=2)
         self.enc2 = RetinexBlock(32, 64, stride=2)
@@ -187,7 +187,7 @@ class MultiScaleRetinexNet(nn.Module):
         self.bottleneck = nn.Sequential(
             RetinexBlock(64, 64),
             CBAM(64),
-            RetinexBlock(64, 64),
+            # RetinexBlock(64, 64),
         )
 
         self.dec2 = UpBlock(64, 32)
