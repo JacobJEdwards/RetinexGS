8bc978459f3f024025db01d189a59eb901eae2ff
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-08-24 15:12:57 +0200

    conf

--- examples/config.py
+++ examples/config.py
@@ -118,7 +118,7 @@ class Config:
     loss_histogram: bool = True
     loss_perceptual_color: bool = True
     loss_dark_preservation: bool = False
-    loss_color_preservation: bool = False
+    loss_color_preservation: bool = True
     loss_chromaticity: bool = False
 
     postfix: str = "_multiexposure"
