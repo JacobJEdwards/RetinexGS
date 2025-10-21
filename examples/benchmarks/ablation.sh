06e3b91b4f35726693d0edd276e4d58a6d2727f5
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-10-21 07:30:16 +0100

    ablation

--- examples/benchmarks/ablation.sh
+++ examples/benchmarks/ablation.sh
@@ -4,44 +4,43 @@ POSTFIXES="variance multiexposure contrast"
 
 RESULT_DIR="/workspace/3d"
 
-for POSTFIX in $POSTFIXES; do
-  for SCENE in $SCENE_LIST;
-  do
-
-      if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
-          DATA_FACTOR=2
-      else
-          DATA_FACTOR=4
-      fi
-
-      echo "Running $SCENE on $POSTFIX"
-
-      CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-          --data_dir $SCENE_DIR/"$SCENE"/ \
-          --postfix "$POSTFIX" \
-          --save_images \
-          --tb_save_image \
-          --result_dir $RESULT_DIR/"$POSTFIX"/"$SCENE"/
-  done
-done
+#for POSTFIX in $POSTFIXES; do
+#  for SCENE in $SCENE_LIST;
+#  do
+#
+#      if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+#          DATA_FACTOR=2
+#      else
+#          DATA_FACTOR=4
+#      fi
+#
+#      echo "Running $SCENE on $POSTFIX"
+#
+#      CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+#          --data_dir $SCENE_DIR/"$SCENE"/ \
+#          --postfix "$POSTFIX" \
+#          --save_images \
+#          --tb_save_image \
+#          --result_dir $RESULT_DIR/"$POSTFIX"/"$SCENE"/
+#  done
+#done
 
 CONFIG_OPTIONS=(
-    "--uncertainty_weighting"
+    "--lambda-exclusion 0.0 --lambda-shn-reg 0.0"
     "--no-use-normals"
     "--no-use-view-dirs"
     "--no-use-camera-response-network"
-    "--lambda_exclusion 0.0"
-    "--lambda_shn_reg 0.0"
-    "--learning_steps 10000"
+    "--lambda_illum_smoothness 1.0"
+    "--lambda_tv_loss 1.0"
+
 )
 CONFIG_NAMES=(
-    "use_uncertainty_weighting"
+    "no_loss_terms"
     "no_normals"
     "no_view_dirs"
     "no_crn"
-    "no_exclusion"
-    "no_shn_reg"
-    "no_freeze_net"
+    "with_smoothness"
+    "with_tv_loss"
 )
 
 for POSTFIX in $POSTFIXES; do
