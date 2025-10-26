fa59c81014e211c8ff3946668937a860180bb0c1
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-10-26 16:20:12 +0000

    ablation

--- examples/benchmarks/ablation.sh
+++ examples/benchmarks/ablation.sh
@@ -2,28 +2,28 @@ SCENE_DIR="/workspace/360_v2"
 SCENE_LIST="garden bicycle stump bonsai counter kitchen room" # treehill flowers
 POSTFIXES="variance multiexposure contrast"
 
-RESULT_DIR="/workspace/3d"
+RESULT_DIR_BASE="/workspace/3d"
 
-for POSTFIX in $POSTFIXES; do
-  for SCENE in $SCENE_LIST;
-  do
-
-      if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
-          DATA_FACTOR=2
-      else
-          DATA_FACTOR=4
-      fi
-
-      echo "Running $SCENE on $POSTFIX"
-
-      CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
-          --data_dir $SCENE_DIR/"$SCENE"/ \
-          --postfix "$POSTFIX" \
-          --save_images \
-          --tb_save_image \
-          --result_dir $RESULT_DIR/"$POSTFIX"/"$SCENE"/
-  done
-done
+#for POSTFIX in $POSTFIXES; do
+#  for SCENE in $SCENE_LIST;
+#  do
+#
+#      if [ "$SCENE" = "bonsai" ] || [ "$SCENE" = "counter" ] || [ "$SCENE" = "kitchen" ] || [ "$SCENE" = "room" ]; then
+#          DATA_FACTOR=2
+#      else
+#          DATA_FACTOR=4
+#      fi
+#
+#      echo "Running $SCENE on $POSTFIX"
+#
+#      CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer --data_factor $DATA_FACTOR \
+#          --data_dir $SCENE_DIR/"$SCENE"/ \
+#          --postfix "$POSTFIX" \
+#          --save_images \
+#          --tb_save_image \
+#          --result_dir $RESULT_DIR/"$POSTFIX"/"$SCENE"/
+#  done
+#done
 
 CONFIG_OPTIONS=(
     "--lambda-exclusion 0.0 --lambda-shn-reg 0.0"
@@ -44,21 +44,21 @@ CONFIG_NAMES=(
 )
 
 for POSTFIX in $POSTFIXES; do
-    RESULT_DIR_BASE="/workspace/3d/ablation/$POSTFIX"
+    RESULT_DIR_ABLATION="$RESULT_DIR_BASE/ablation/$POSTFIX"
 
     for SCENE in $SCENE_LIST;
     do
         for i in "${!CONFIG_OPTIONS[@]}"; do
             echo "Running $SCENE with ${CONFIG_NAMES[$i]} on $POSTFIX"
-            RESULT_DIR="$RESULT_DIR_BASE/${CONFIG_NAMES[$i]}"
+            RESULT_DIR="$RESULT_DIR_ABLATION/${CONFIG_NAMES[$i]}"
             CUDA_VISIBLE_DEVICES=0 python simple_trainer.py --disable_viewer \
                 --data_dir $SCENE_DIR/"$SCENE"/ \
-                --postfix $POSTFIX \
+                --postfix "$POSTFIX" \
                 --no_save_images \
                 --no_save_ckpt \
                 --no_tb_save_image \
                 --tb_every 1000 \
-                --result_dir $RESULT_DIR/"$SCENE"/ \
+                --result_dir "$RESULT_DIR"/"$SCENE"/ \
                 ${CONFIG_OPTIONS[$i]}
         done
     done
