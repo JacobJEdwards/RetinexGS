34d49dcfdb39b06101594ca58c98b294ebdb7e45
JACOB EDWARDS <jacob.john1@talktalk.net>
2026-02-12 21:53:20 +0000

    Load train test

---
+++ examples/run_tests.sh
@@ -0,0 +1,24 @@
+python simple_trainer.py default --data-dir /workspace/datasets/log --result-dir /workspace/datasets/log/results/gsplat
+python simple_trainer.py default --data-dir /workspace/datasets/log --result-dir /workspace/datasets/log/results/bilateral_grid --post-processing bilateral_grid
+python simple_trainer.py default --data-dir /workspace/datasets/log --result-dir /workspace/datasets/log/results/ppisp --post-processing ppisp
+python simple_trainer.py default --data-dir /workspace/datasets/log --result-dir /workspace/datasets/log/results/gs-w --app-opt
+
+python simple_trainer.py default --data-dir /workspace/datasets/Log-hicontrast --result-dir /workspace/datasets/Log-hicontrast/results/gsplat
+python simple_trainer.py default --data-dir /workspace/datasets/Log-hicontrast --result-dir /workspace/datasets/Log-hicontrast/results/bilateral_grid --post-processing bilateral_grid
+python simple_trainer.py default --data-dir /workspace/datasets/Log-hicontrast --result-dir /workspace/datasets/Log-hicontrast/results/ppisp --post-processing ppisp
+python simple_trainer.py default --data-dir /workspace/datasets/Log-hicontrast --result-dir /workspace/datasets/Log-hicontrast/results/gs-w --app-opt
+
+python simple_trainer.py default --data-dir /workspace/datasets/plastic-cart-130deg --result-dir /workspace/datasets/plastic-cart-130deg/results/gsplat
+python simple_trainer.py default --data-dir /workspace/datasets/plastic-cart-130deg --result-dir /workspace/datasets/plastic-cart-130deg/results/bilateral_grid --post-processing bilateral_grid
+python simple_trainer.py default --data-dir /workspace/datasets/plastic-cart-130deg --result-dir /workspace/datasets/plastic-cart-130deg/results/ppisp --post-processing ppisp
+python simple_trainer.py default --data-dir /workspace/datasets/plastic-cart-130deg --result-dir /workspace/datasets/plastic-cart-130deg/results/gs-w --app-opt
+
+python simple_trainer.py default --data-dir /workspace/datasets/robot-recapture --result-dir /workspace/datasets/robot-recapture/results/gsplat
+python simple_trainer.py default --data-dir /workspace/datasets/robot-recapture --result-dir /workspace/datasets/robot-recapture/results/bilateral_grid --post-processing bilateral_grid
+python simple_trainer.py default --data-dir /workspace/datasets/robot-recapture --result-dir /workspace/datasets/robot-recapture/results/ppisp --post-processing ppisp
+python simple_trainer.py default --data-dir /workspace/datasets/robot-recapture --result-dir /workspace/datasets/robot-recapture/results/gs-w --app-opt
+
+python simple_trainer.py default --data-dir /workspace/datasets/Rocks2 --result-dir /workspace/datasets/Rocks2/results/gsplat
+python simple_trainer.py default --data-dir /workspace/datasets/Rocks2 --result-dir /workspace/datasets/Rocks2/results/bilateral_grid --post-processing bilateral_grid
+python simple_trainer.py default --data-dir /workspace/datasets/Rocks2 --result-dir /workspace/datasets/Rocks2/results/ppisp --post-processing ppisp
+python simple_trainer.py default --data-dir /workspace/datasets/Rocks2 --result-dir /workspace/datasets/Rocks2/results/gs-w --app-opt
