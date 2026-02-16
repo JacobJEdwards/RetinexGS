91848e506d1ea6e1a1c044c9c09edf695d2e185f
JACOB EDWARDS <jacob.john1@talktalk.net>
2026-02-16 08:48:03 +0000

    Load train test

---
+++ examples/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASE="/workspace/datasets"
+
+DATASETS=(
+    "Circular-bench"
+    "chair"
+    "4logs-earlham-park"
+    "plastic-cart-130-vert-shade"
+    "robot-under-over-exposure"
+    "Rocks2"
+    "robot-recapture"
+    "plastic-cart-130deg"
+    "Log-hicontrast"
+    "log"
+)
+
+for DATA in "${DATASETS[@]}"; do
+    echo "Processing: $DATA"
+    DIR="$BASE/$DATA"
+
+    python simple_trainer.py default --data-dir "$DIR" --result-dir "$DIR/results/2d"
+done
