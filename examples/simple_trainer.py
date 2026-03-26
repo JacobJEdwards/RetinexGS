0adc17dfa63ad2a4059bc198ea94897adb796cab
JACOB EDWARDS <jacob.john1@talktalk.net>
2026-03-26 17:56:00 +0000

    increase eval steps

--- examples/simple_trainer.py
+++ examples/simple_trainer.py
@@ -1379,10 +1379,10 @@ def objective(trial: optuna.Trial, cfg: Config) -> float:
     cfg.save_images = False
     cfg.tb_save_image = False
     cfg.disable_video = True
-    cfg.tb_every = 10000
+    cfg.tb_every = 100000
 
-    cfg.max_steps = 4000
-    cfg.eval_steps = [1000, 2000, 3000, 4000]
+    cfg.max_steps = 10000
+    cfg.eval_steps = [2000, 5000, 8000, 10_000]
 
     cfg.means_lr = trial.suggest_float("means_lr", 1e-5, 1e-3, log=True)
     cfg.scales_lr = trial.suggest_float("scales_lr", 1e-4, 5e-2, log=True)
