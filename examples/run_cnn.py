bba87be475553cd0e455fc783a108e0706dfbd5c
JACOB EDWARDS <jacob.john1@talktalk.net>
2026-04-03 15:29:01 +0100

    no perceptuable colour permute

--- examples/run_cnn.py
+++ examples/run_cnn.py
@@ -147,7 +147,7 @@ def main(cfg: Config):
                 total_loss += cfg.lambda_histogram * histogram_loss(reflectance_map, target_histogram_dist)
 
             if cfg.loss_perceptual_color:
-                total_loss += cfg.lambda_perceptual_color * loss_perceptual_colour(reflectance_map, pixels)
+                total_loss += cfg.lambda_perceptual_color * loss_perceptual_colour(reflectance_map, input_image_for_net)
 
             if cfg.loss_variance:
                 illum_std = torch.std(illumination_map, dim=[2, 3])
